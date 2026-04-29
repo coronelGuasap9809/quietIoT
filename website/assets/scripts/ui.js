5fe5e31d30f7d870df44dac2213f905b1b478ab1
Emiliano Huerta <emilianohuertanunez@education.nsw.gov.au>
2026-04-29 18:15:16 +1000

    refactor theme and motion settings: update class names and add motion controls

--- website/assets/scripts/ui.js
+++ website/assets/scripts/ui.js
@@ -1,8 +1,16 @@
 let theme = localStorage.getItem("theme")
+let motionSetting = localStorage.getItem("motionSetting")
+
 const themeButtons = document.querySelectorAll(".theme-btn")
+const motionButtons = document.querySelectorAll(".motion-btn")
+const contentElements = document.querySelectorAll(".content")
 
 const clearActiveThemeButtons = () => {
     themeButtons.forEach(button => button.classList.remove("active"))
+}
+
+const clearActiveMotionButtons = () => {
+    motionButtons.forEach(button => button.classList.remove("active"))
 }
 
 const enableLightmode = () => {
@@ -29,12 +37,35 @@ const enableAutomode = () => {
     localStorage.setItem("theme", "auto")
 }
 
+const enableMotion = () => {
+    contentElements.forEach(element => element.classList.remove("disable-motion"))
+    clearActiveMotionButtons()
+    document.getElementById("enable-motion").classList.add("active")
+    localStorage.setItem("motionSetting", "enabled")
+}
+
+const disableMotion = () => {
+    contentElements.forEach(element => element.classList.add("disable-motion"))
+    clearActiveMotionButtons()
+    document.getElementById("disable-motion").classList.add("active")
+    localStorage.setItem("motionSetting", "disabled")
+}
+
+
+/* Read localStorage theme data */
 if (theme === "light") {
     enableLightmode()
 } else if (theme === "dark") {
     enableDarkmode()
 } else {
     enableAutomode()
+}
+
+/* Read localStorage motion data */
+if (motionSetting === "disabled") {
+    disableMotion()
+} else {
+    enableMotion()
 }
 
 themeButtons.forEach(button => {
@@ -47,6 +78,18 @@ themeButtons.forEach(button => {
             enableDarkmode()
         } else {
             enableAutomode()
+        } 
+    })
+})
+
+motionButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        motionSetting = localStorage.getItem("motionSetting")
+        const id = event.target.id
+        if (id === "disable-motion") {
+            disableMotion()
+        } else {
+            enableMotion()
         }
     })
 })
